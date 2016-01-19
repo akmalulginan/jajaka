2b78c85476adbcb6476d3bb49e23f5dece864539
Agta01 <hendarsundana@gmail.com>
2016-01-20 03:21:58 +0700

    Merge origin/master
    Conflicts:
    	Tropika_rev2/lib/database.db
    	Tropika_rev2/src/view/SupplierPanel.form
    	Tropika_rev2/src/view/SupplierPanel.java

--- Tropika_rev2/src/view/KategoriPanel.java
+++ Tropika_rev2/src/view/KategoriPanel.java
@@ -54,6 +54,7 @@ public class KategoriPanel extends javax.swing.JPanel {
         dataTable = new javax.swing.JTable();
         inputCepatKey5 = new javax.swing.JButton();
         kategoriComboBox = new javax.swing.JComboBox();
+        jLabel1 = new javax.swing.JLabel();
 
         cariText.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         cariText.setForeground(new java.awt.Color(40, 40, 40));
@@ -117,6 +118,8 @@ public class KategoriPanel extends javax.swing.JPanel {
 
         kategoriComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Stok", "Non Stok" }));
 
+        jLabel1.setText("jLabel1");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -132,18 +135,26 @@ public class KategoriPanel extends javax.swing.JPanel {
                         .addComponent(kategoriComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(inputCepatKey5, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(88, 88, 88)
+                        .addComponent(jLabel1)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(inputCepatKey5, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(kategoriComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(inputCepatKey5, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(kategoriComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(24, 24, 24))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)))
                 .addComponent(table, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(47, 47, 47))
         );
@@ -159,6 +170,7 @@ public class KategoriPanel extends javax.swing.JPanel {
     private javax.swing.JTextField cariText;
     private javax.swing.JTable dataTable;
     private javax.swing.JButton inputCepatKey5;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JComboBox kategoriComboBox;
