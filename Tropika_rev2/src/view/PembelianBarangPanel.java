c4edbadc08c0e437b7b8346c4e3e94b3fb7f62a3
Agta01 <hendarsundana@gmail.com>
2016-02-05 01:49:06 +0700

    Merge origin/master
    Conflicts:
    	Tropika_rev2/src/control/DataKaryawanControl.java

--- Tropika_rev2/src/view/PembelianBarangPanel.java
+++ Tropika_rev2/src/view/PembelianBarangPanel.java
@@ -1201,6 +1201,7 @@ public class PembelianBarangPanel extends javax.swing.JPanel {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        new PilihSupplierDialog(new javax.swing.JFrame(), true).setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
 
